476ea7bb31e04fb8eed96428eedbc788f49ba7c5
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-11-13 01:35:44 +0300

    Add missing fields

--- chat.go
+++ chat.go
@@ -1,29 +1,45 @@
 package initdata
 
 const (
+	ChatTypeSender     ChatType = "sender"
+	ChatTypePrivate    ChatType = "private"
 	ChatTypeGroup      ChatType = "group"
 	ChatTypeSupergroup ChatType = "supergroup"
 	ChatTypeChannel    ChatType = "channel"
 )
 
 // ChatType describes type of chat.
-type ChatType = string
+type ChatType string
+
+// Known returns true if current chat type is known.
+func (c ChatType) Known() bool {
+	switch c {
+	case ChatTypeSender,
+		ChatTypePrivate,
+		ChatTypeGroup,
+		ChatTypeSupergroup,
+		ChatTypeChannel:
+		return true
+	default:
+		return false
+	}
+}
 
 // Chat describes chat information:
-// https://core.telegram.org/bots/webapps#webappchat
+// https://docs.telegram-mini-apps.com/launch-parameters/init-data#chat
 type Chat struct {
 	// Unique identifier for this chat.
 	ID int64 `json:"id"`
-
-	// Optional. URL of the chat’s photo. The photo can be in .jpeg or .svg
-	// formats. Only returned for Web Apps launched from the attachment menu.
-	PhotoURL string `json:"photo_url"`
 
 	// Type of chat.
 	Type ChatType `json:"type"`
 
 	// Title of the chat.
 	Title string `json:"title"`
+
+	// Optional. URL of the chat’s photo. The photo can be in .jpeg or .svg
+	// formats. Only returned for Web Apps launched from the attachment menu.
+	PhotoURL string `json:"photo_url"`
 
 	// Optional. Username of the chat.
 	Username string `json:"username"`
