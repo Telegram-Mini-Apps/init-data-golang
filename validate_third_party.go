29163138f1c57deadf3e6c36976c4215b70aec46
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-12-15 21:23:56 +0500

    feat: implement third party validation

---
+++ validate_third_party.go
@@ -0,0 +1,122 @@
+package initdata
+
+import (
+	"crypto/ed25519"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"net/url"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	_telegramProdPublicKey, _ = hex.DecodeString("e7bf03a2fa4602af4580703d88dda5bb59f32ed8b02a56c187fe7d34caed242d")
+	_telegramTestPublicKey, _ = hex.DecodeString("40055058a4ee38156a06562e52eece92a771bcd8346a8c4615cb7376eddf72ec")
+)
+
+// ValidateThirdPartyWithEnv validates passed init data assuming that it was signed by Telegram.
+// This method expects initData to be passed in the exact raw format as it could be found
+// in window.Telegram.WebApp.initData.
+//
+// Returns error if something is wrong with the passed init data. Nil otherwise.
+//
+// initData - init data passed from application;
+// botID - init data Telegram Bot issuer identifier;
+// expIn - maximum init data lifetime. It is strongly recommended to use this
+// parameter. In case, exp duration is less than or equal to 0, function does
+// not check if parameters are expired.
+// isProd - true if the init data was issued in Telegram production environment;
+func ValidateThirdPartyWithEnv(
+	initData string,
+	botID int64,
+	expIn time.Duration,
+	isProd bool,
+) error {
+	// Parse passed init data as query string.
+	q, err := url.ParseQuery(initData)
+	if err != nil {
+		return ErrUnexpectedFormat
+	}
+
+	var (
+		// Init data creation time.
+		authDate time.Time
+		// Init data signature.
+		signature []byte
+		// All found key-value pairs.
+		pairs = make([]string, 0, len(q))
+	)
+
+	// Iterate over all key-value pairs of parsed parameters.
+	for k, v := range q {
+		// hash is ignored during this validation.
+		if k == "hash" {
+			continue
+		}
+
+		// Store found signature.
+		if k == "signature" {
+			signature, _ = base64.URLEncoding.DecodeString(
+				// Signature is base64-encoded. We append padding by ourselves as long Telegram's server
+				// incorrectly creates a base64 string, but GoLang intolerant to this and requires strict
+				// format compliance.
+				v[0] + strings.Repeat("=", 4-len(v[0])%4),
+			)
+			continue
+		}
+		if k == "auth_date" {
+			if i, err := strconv.Atoi(v[0]); err == nil {
+				authDate = time.Unix(int64(i), 0)
+			}
+		}
+		// Append new pair.
+		pairs = append(pairs, k+"="+v[0])
+	}
+
+	// Signature is always required.
+	if len(signature) == 0 {
+		return ErrSignMissing
+	}
+
+	// In case, expiration time is passed, we do additional parameters check.
+	if expIn > 0 {
+		// In case, auth date is zero, it means, we can not check if parameters
+		// are expired.
+		if authDate.IsZero() {
+			return ErrAuthDateMissing
+		}
+
+		// Check if init data is expired.
+		if authDate.Add(expIn).Before(time.Now()) {
+			return ErrExpired
+		}
+	}
+
+	// According to docs, we sort all the pairs in alphabetical order.
+	sort.Strings(pairs)
+
+	var publicKey []byte
+	if isProd {
+		publicKey = _telegramProdPublicKey
+	} else {
+		publicKey = _telegramTestPublicKey
+	}
+
+	if !ed25519.Verify(
+		publicKey,
+		[]byte(fmt.Sprintf("%d:WebAppData\n%s", botID, strings.Join(pairs, "\n"))),
+		signature,
+	) {
+		return ErrSignInvalid
+	}
+	return nil
+}
+
+// ValidateThirdParty performs validation described in the Validate3rdWithEnv function using
+// production environment.
+func ValidateThirdParty(initData string, botID int64, expIn time.Duration) error {
+	return ValidateThirdPartyWithEnv(initData, botID, expIn, true)
+}
---
+++ validate_third_party_test.go
@@ -0,0 +1,54 @@
+package initdata
+
+import (
+	"errors"
+	"testing"
+	"time"
+)
+
+const (
+	_validateThirdPartyTestInitData = "user=%7B%22id%22%3A279058397%2C%22first_name%22%3A%22Vladislav%20%2B%20-%20%3F%20%5C%2F%22%2C%22last_name%22%3A%22Kibenko%22%2C%22username%22%3A%22vdkfrost%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2F4FPEE4tmP3ATHa57u6MqTDih13LTOiMoKoLDRG4PnSA.svg%22%7D&chat_instance=8134722200314281151&chat_type=private&auth_date=1733584787&hash=2174df5b000556d044f3f020384e879c8efcab55ddea2ced4eb752e93e7080d6&signature=zL-ucjNyREiHDE8aihFwpfR9aggP2xiAo3NSpfe-p7IbCisNlDKlo7Kb6G4D0Ao2mBrSgEk4maLSdv6MLIlADQ"
+	_validateThirdPartyBotID        = 7342037359
+)
+
+func TestValidateThirdPartyValid(t *testing.T) {
+	err := ValidateThirdParty(_validateThirdPartyTestInitData, _validateThirdPartyBotID, 0)
+	if err != nil {
+		t.Errorf("expected not to return error. Received: %q", err)
+	}
+}
+
+func TestValidateThirdPartyExpired(t *testing.T) {
+	err := ValidateThirdParty(_validateThirdPartyTestInitData, _validateThirdPartyBotID, time.Second)
+	if !errors.Is(err, ErrExpired) {
+		t.Errorf("expected to receive %q. Received: %q", ErrExpired, err)
+	}
+}
+
+func TestValidateThirdPartyInvalidFormat(t *testing.T) {
+	err := ValidateThirdParty("here comes something wrong;", 1, 0)
+	if !errors.Is(err, ErrUnexpectedFormat) {
+		t.Errorf("expected to receive %q. Received: %q", ErrUnexpectedFormat, err)
+	}
+}
+
+func TestValidateThirdPartySignMissing(t *testing.T) {
+	err := ValidateThirdParty("no_signature=true", 1, 0)
+	if !errors.Is(err, ErrSignMissing) {
+		t.Errorf("expected to receive %q. Received: %q", ErrSignMissing, err)
+	}
+}
+
+func TestValidateThirdPartyAuthDateMissing(t *testing.T) {
+	err := ValidateThirdParty("signature=abc", 1, time.Second)
+	if !errors.Is(err, ErrAuthDateMissing) {
+		t.Errorf("expected to receive %q. Received: %q", ErrAuthDateMissing, err)
+	}
+}
+
+func TestValidateThirdPartySignInvalid(t *testing.T) {
+	err := ValidateThirdParty(_validateThirdPartyTestInitData+"a", 1, 0)
+	if !errors.Is(err, ErrSignInvalid) {
+		t.Errorf("expected to receive %q. Received: %q", ErrSignInvalid, err)
+	}
+}
